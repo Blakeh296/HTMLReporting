c72b2e9e22f2b184c1b1e50908013f85d51491a5
Blake Hoelle <bhoelle216@gmail.com>
2018-10-09 20:22:47 -0400

    Added Methods to DLL

--- HTMLReportingProject/HTMLReportingProject/Class1.cs
+++ HTMLReportingProject/HTMLReportingProject/Class1.cs
@@ -18,6 +18,45 @@ namespace HTMLReportingProject
             set { _Name = value; }
         }
 
+        public HTMLReportor() //For when report had no name
+        {
+            _Name = "index";
+            _folder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\";
+            CreateNewProject();
+        }
+
+        public HTMLReportor(string ProjectName)
+        {
+            _Name = ProjectName;
+            _folder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\";
+        }
+
+        public HTMLReportor(string ProjectName, string Folder )
+        {
+            _Name = ProjectName;
+
+            //Check to see if folder exists, if it doesnt create it
+            if(!Directory.Exists(Folder))
+            {
+                Directory.CreateDirectory(Folder);
+            }
+
+            //If the folder already exists, use it 
+            if(Directory.Exists(Folder))
+            {
+                if (!Folder.EndsWith("\\"))
+                    Folder += "\\";
+
+                _folder = Folder;
+            }
+            else
+            {
+                _folder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\";
+            }
+
+            CreateNewProject();
+        }
+
         private void CreateNewProject()
         {
             try
@@ -48,6 +87,34 @@ namespace HTMLReportingProject
                 //Close out the body
                 _Body.Enqueue("</body>");
                 _Body.Enqueue("</html>");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void WriteHTMLProject()
+        {
+            StreamWriter writeHTML = new StreamWriter(_folder + ProjectName + ".html");
+
+            try
+            {
+                CloseProject();
+
+                //Write header and body queues to file
+                while(_Header.Count > 0)
+                {
+                    writeHTML.WriteLine(_Header.Dequeue());
+                }
+
+                while(_Body.Count > 0)
+                {
+                    writeHTML.WriteLine(_Body.Dequeue());
+                }
+
+                writeHTML.Flush();
+                writeHTML.Close();
             }
             catch (Exception ex)
             {
